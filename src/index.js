fac7730c00a79d5d206225fd192a1dfb125e7a0d
boski <boskitechh@gmail.com>
2022-08-12 13:13:10 +0100

    Added locomotive scroll

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { gsap } from "gsap";
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll();
 
 gsap.to("h1", {stagger:1, rotation:360, fill:"yellow", duration:3})
